0e80fa9c973f1be7c0995cb0a65b6ff736d5cc87
Adarsh L K <erlkadarsh@gmail.com>
2025-02-21 20:27:06 +0530

    Add routes for User 🎯

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,12 @@
 import express from "express";
+import upload from "../middlewares/multer.js";
+import multerErrorHandler from "../middlewares/errorHandler.js";
+import { signup, login, logout } from "../controllers/User.controller.js";
 
 const router = express.Router();
 
-
+router.post("/signup", upload.single("image"), multerErrorHandler, signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
