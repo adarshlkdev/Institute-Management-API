8cc03250d8f88297b8d30f1f2dc20f7a5b62d386
Adarsh L K <erlkadarsh@gmail.com>
2025-02-22 09:48:27 +0530

    Add route for fee ⚡

--- routes/fee.routes.js
+++ routes/fee.routes.js
@@ -1,7 +1,14 @@
 import express from "express";
+import { checkAuth } from "../middlewares/checkAuth.js";
+import { addFee, paymentHistory } from "../controllers/Fee.controller.js";
+import { getFeeIntoCourse } from "../controllers/Fee.controller.js";
 
 const router = express.Router();
 
+router.post("/add", checkAuth, addFee);
 
+router.get("/history", checkAuth, paymentHistory);
+
+router.get("/getFeeIntoCourse", checkAuth, getFeeIntoCourse);
 
 export default router;
