2840f78d7b8e47a5031b3b9469843012728ed4b5
Adarsh L K <erlkadarsh@gmail.com>
2025-02-22 09:49:01 +0530

    Final changes for server.js 🚀

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./db/connection.js";
-import batchRoutes from "./routes/batch.routes.js";
+import courseRoutes from "./routes/course.routes.js";
 import feeRoutes from "./routes/fee.routes.js";
 import studentRoutes from "./routes/student.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -11,11 +11,12 @@ dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/batch", batchRoutes);
+app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/fee", feeRoutes);
 app.use("/api/v1/student", studentRoutes);
-app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/auth", userRoutes);
 
 app.use("*", (req, res) => {
   res.status(404).json({ success: false, message: "Page not found" });
