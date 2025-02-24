dad951500e70612cad4e1b16543b917269538451
Adarsh L K <erlkadarsh@gmail.com>
2025-02-24 23:21:38 +0530

    Fix some issues 🚀

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -2,9 +2,14 @@ import jwt from "jsonwebtoken";
 
 export const checkAuth = (req, res, next) => {
   try {
-    // Get the token from the request header
+    if (!req.headers.authorization) {
+      return res.status(401).json({
+        success: false,
+        message: "No Token provided",
+      });
+    }
     const token = req.headers.authorization.split(" ")[1];
-    // Verify the token
+
     if (!token) {
       return res.status(401).json({
         success: false,
@@ -18,9 +23,8 @@ export const checkAuth = (req, res, next) => {
         message: "Unauthorized access.",
       });
     }
-    // Attach the decoded token to the request object
+
     req.userData = decoded;
-    // Pass control to the next middleware
     next();
   } catch (error) {
     return res.status(401).json({
