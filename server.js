681bf1bd4ab6e98062082091c166d4a304e0eaf3
Adarsh L K <129508819+adarshlkdev@users.noreply.github.com>
2025-04-13 23:43:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import userRoutes from "./routes/user.routes.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
